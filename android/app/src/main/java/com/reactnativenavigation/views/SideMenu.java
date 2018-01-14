9375b43293a8eecc3a93c70d8d738cec68a14b9a
Elegode <cedric.yang@ihealthlabs.eu>
2018-01-14 22:38:33 +0100

    fix import

--- android/app/src/main/java/com/reactnativenavigation/views/SideMenu.java
+++ android/app/src/main/java/com/reactnativenavigation/views/SideMenu.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 
+import com.facebook.react.bridge.Callback;
 import com.reactnativenavigation.NavigationApplication;
 import com.reactnativenavigation.params.BaseScreenParams;
 import com.reactnativenavigation.params.SideMenuParams;
